21c115f7fc5d4a88565f1765c0684332fb674f3b
Jan-CT <38039641+Jan-CT@users.noreply.github.com>
2018-04-05 16:42:46 +0200

    Add gain and loss

--- r_scripts/001_get_sample_data.R
+++ r_scripts/001_get_sample_data.R
@@ -21,21 +21,88 @@ for (i in 1:nrow(sample_data)) {
   if (deck == 1)  {
     
     if (value == 1) {
-      sample_data[i, 7] <- 200
-      sample_data[i, 8] <- 150
-    } else if {
-      
-      
+      sample_data[i, 7] <- 100
+      sample_data[i, 8] <- 0
+    } else if (value == 2) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- 0
+    } else if (value == 3) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- 0  
+    } else if (value == 4) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- 0
+    } else if (value == 5) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- 0 
+    } else if (value == 6) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- -150
+    } else if (value == 7) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- -200
+    } else if (value == 8) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- -250
+    } else if (value == 9) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- -300
+    } else if (value == 10) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- -350
     }
 
   } else if (deck == 2) {
     
+    if (value == 1) {
+      sample_data[i, 7] <- 50
+      sample_data[i, 8] <- 0
+    } else if (value == 2) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- -250
     
+      
   } else if (deck == 3) {
-    
+   
+    if (value == 1) {
+      sample_data[i, 7] <- 50
+      sample_data[i, 8] <- 0
+    } else if (value == 2) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- 0
+    } else if (value == 3) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- 0  
+    } else if (value == 4) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- 0
+    } else if (value == 5) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- 0 
+    } else if (value == 6) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- 0
+    } else if (value == 7) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- 0
+    } else if (value == 8) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- -25
+    } else if (value == 9) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- -50
+    } else if (value == 10) {
+      sample_data[i,7] <- 50
+      sample_data[i,8] <- -75
     
   } else if (deck == 4) {
     
+    if (value == 1) {
+      sample_data[i, 7] <- 100
+      sample_data[i, 8] <- 0
+    } else if (value == 2) {
+      sample_data[i,7] <- 100
+      sample_data[i,8] <- -1250
     
   }
 
